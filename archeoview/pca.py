cd6ecaccd1d3551fdfbd197fed4d091efc9c53ef
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2020-05-14 13:09:12 +0200

    Add min-max scaling to final pca image

--- archeoview/pca.py
+++ archeoview/pca.py
@@ -5,7 +5,10 @@ from sklearn.decomposition import PCA
 
 
 def pca_decomposition(
-    image: np.ndarray, n_dimensions: int = 3, bands_first: bool = False
+    image: np.ndarray,
+    n_dimensions: int = 3,
+    bands_first: bool = False,
+    normalise: bool = True,
 ) -> Tuple[np.ndarray, float]:
     """Applies Principal Component Analysis on the image and returns the number of components required
 
@@ -15,9 +18,10 @@ def pca_decomposition(
     Keyword Arguments:
         n_dimensions -- Number of dimensions to return from PCA (default: {3})
         bands_first -- If true, axes of image are in this order: (bands, height, width) (default: {False})
+        normalise -- If image output should be normalised between [0, 1] with min-max scaling (default: {True})
 
     Returns:
-        A tuple with the output image with shape (height, width, n_dimensions) and the explained variance 
+        A tuple with the output image with shape (height, width, n_dimensions) and the explained variance
         ratio of PCA, i.e. how much variance is explained by these n_dimensions
     """
 
@@ -36,5 +40,13 @@ def pca_decomposition(
 
     # Back to 2D image
     pca_image = flattened_pca.reshape(height, width, n_dimensions)
+
+    # Possibly normalise between [0, 1]
+    if normalise:
+        for band_idx in range(n_dimensions):
+            min_band = pca_image[:, :, band_idx].min()
+            max_band = pca_image[:, :, band_idx].max()
+            pca_image[:, :, band_idx] -= min_band
+            pca_image[:, :, band_idx] /= max_band - min_band
 
     return pca_image, pca.explained_variance_ratio_
