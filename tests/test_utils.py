81cc2f815169eec02464bee601df2f6d316b8493
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2020-05-24 09:59:23 +0200

    Adapted tests to changes, plus added some edge cases

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from archeoview.utils import (
     interpolate,
-    interpolate_bands,
+    interpolate_or_filter_bands,
     upscale,
     geotiff_to_numpy,
     minmax_scaling,
@@ -28,29 +28,38 @@ def test_interpolate():
     ), "Should be able to scale very weird shapes"
 
 
-def test_interpolate_bands():
-    assert interpolate_bands([]).shape == (
-        0,
-    ), "If given no bands should return no bands"
+def test_interpolate_or_filter_bands():
+    _, empty_bands = interpolate_or_filter_bands([], [])
+    assert empty_bands.shape == (0,), "If given no bands should return no bands"
 
     test_bands = [np.random.rand(14, 20)] * 3
-    assert interpolate_bands(test_bands).shape == (
+    test_names = [""] * 3
+    _, basic_test = interpolate_or_filter_bands(test_names, test_bands)
+    assert basic_test.shape == (
         3,
         14,
         20,
     ), "Should return np.ndarray with correct shape"
 
     test_bands2 = [np.random.rand(14, 20)] * 3 + [np.random.rand(12, 15)] * 2
-    assert interpolate_bands(test_bands2).shape == (
-        5,
-        14,
-        20,
-    ), "Should return all bands interpolated correctly to a certain dimension"
-    assert interpolate_bands(test_bands2, interpolation=False).shape == (
+    test_names2 = [""] * 5
+    _, filtered_test2 = interpolate_or_filter_bands(
+        test_names2, test_bands2, interpolation=False
+    )
+    assert filtered_test2.shape == (
         3,
         14,
         20,
     ), "Should return all high-res bands and skip lower res bands"
+
+    _, interpolated_test2 = interpolate_or_filter_bands(
+        test_names2, test_bands2, interpolation=True
+    )
+    assert interpolated_test2.shape == (
+        5,
+        14,
+        20,
+    ), "Should return all bands interpolated correctly to a certain dimension"
 
 
 def test_upscale():
@@ -59,6 +68,12 @@ def test_upscale():
     upscaled_image = upscale(image, target_shape)
     assert (
         upscaled_image.shape[:2] == target_shape
+    ), "Should have converted to target shape"
+
+    target_shape2 = image.shape[0] * 8, image.shape[1] * 8
+    upscaled_image2 = upscale(image, target_shape2)
+    assert (
+        upscaled_image2.shape[:2] == target_shape2
     ), "Should have converted to target shape"
 
 
@@ -70,21 +85,27 @@ def test_geotiff_to_numpy():
         len(bands_names) == image.shape[2]
     ), "Names should correspond to number of bands"
 
-    _, image2 = geotiff_to_numpy(
-        "data/20180926-kortgene-multires/", interpolation=False
-    )
+    bands_names2, image2 = geotiff_to_numpy("data/20180926-kortgene-multires/")
     assert image2.shape == (
         174,
         252,
         6,
-    ), "Without interpolation should have only 6 bands"
+    ), "With interpolation to False should have only 6 bands"
+    assert (
+        len(bands_names2) == image2.shape[2]
+    ), "Names should correspond to number of bands"
 
-    _, image3 = geotiff_to_numpy("data/20180926-kortgene-multires/")
+    bands_names3, image3 = geotiff_to_numpy(
+        "data/20180926-kortgene-multires/", interpolation=True
+    )
     assert image3.shape == (
         174,
         252,
         12,
     ), "With interpolation should have all bands upscaled"
+    assert (
+        len(bands_names3) == image3.shape[2]
+    ), "Names should correspond to number of bands"
 
 
 def test_minmax_scaling():
