d2b2e95daffb9359b187b41de3b2362003926eb8
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2020-06-04 19:17:24 +0200

    Comment out Earth Engine test
    Needs auth and I can't get it working in GitHub Actions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -196,72 +196,72 @@ def test_get_image_collection():
     ).all(), "Should correspond to order of given paths"
 
 
-def test_earth_engine_to_google_drive():
-    # We can't really test that stuff is being created inside GDrive,
-    # so we'll just make sure that Tasks are constructed correctly
+# def test_earth_engine_to_google_drive():
+#     # We can't really test that stuff is being created inside GDrive,
+#     # so we'll just make sure that Tasks are constructed correctly
 
-    # Test location
-    point = [4.188454322814947, 52.05157028465844]
-    bbox = [
-        [4.1227080154419005, 52.029502065677526],
-        [4.254887275695807, 52.07542126691518],
-    ]
+#     # Test location
+#     point = [4.188454322814947, 52.05157028465844]
+#     bbox = [
+#         [4.1227080154419005, 52.029502065677526],
+#         [4.254887275695807, 52.07542126691518],
+#     ]
 
-    tasks = earth_engine_to_google_drive(
-        point=point,
-        bounding_box=bbox,
-        start_date="2020-05-15",
-        end_date="2020-05-16",
-        dataset="COPERNICUS/S1_GRD",
-        bands=["VV", "VH"],
-        drive_folder="VeryNiceFolder",
-        task_name="VeryNiceTest",
-        autostart=False,
-    )
+#     tasks = earth_engine_to_google_drive(
+#         point=point,
+#         bounding_box=bbox,
+#         start_date="2020-05-15",
+#         end_date="2020-05-16",
+#         dataset="COPERNICUS/S1_GRD",
+#         bands=["VV", "VH"],
+#         drive_folder="VeryNiceFolder",
+#         task_name="VeryNiceTest",
+#         autostart=False,
+#     )
 
-    assert type(tasks[0]) == ee.batch.Task, "Should have returned ee tasks"
-    drive_options = tasks[0].config["fileExportOptions"]["driveDestination"]
-    assert (
-        drive_options["filenamePrefix"].split("_")[1] == "VeryNiceTest"
-    ), "Correct naming of file"
-    assert drive_options["folder"] == "VeryNiceFolder", "Correct naming of folder"
+#     assert type(tasks[0]) == ee.batch.Task, "Should have returned ee tasks"
+#     drive_options = tasks[0].config["fileExportOptions"]["driveDestination"]
+#     assert (
+#         drive_options["filenamePrefix"].split("_")[1] == "VeryNiceTest"
+#     ), "Correct naming of file"
+#     assert drive_options["folder"] == "VeryNiceFolder", "Correct naming of folder"
 
-    limited_tasks = earth_engine_to_google_drive(
-        point=point,
-        bounding_box=bbox,
-        start_date="2020-05-15",
-        end_date="2020-05-16",
-        dataset="COPERNICUS/S1_GRD",
-        bands=["VV", "VH"],
-        n_images=10,
-        autostart=False,
-    )
-    assert (
-        len(limited_tasks) == 1
-    ), "For this time interval there should be only 1 image"
+#     limited_tasks = earth_engine_to_google_drive(
+#         point=point,
+#         bounding_box=bbox,
+#         start_date="2020-05-15",
+#         end_date="2020-05-16",
+#         dataset="COPERNICUS/S1_GRD",
+#         bands=["VV", "VH"],
+#         n_images=10,
+#         autostart=False,
+#     )
+#     assert (
+#         len(limited_tasks) == 1
+#     ), "For this time interval there should be only 1 image"
 
-    limited_tasks2 = earth_engine_to_google_drive(
-        point=point,
-        bounding_box=bbox,
-        start_date="2020-04-15",
-        end_date="2020-05-16",
-        dataset="COPERNICUS/S1_GRD",
-        bands=["VV", "VH"],
-        n_images=10,
-        autostart=False,
-    )
-    assert len(limited_tasks2) == 10, "Should be limited to 10 images in time period"
+#     limited_tasks2 = earth_engine_to_google_drive(
+#         point=point,
+#         bounding_box=bbox,
+#         start_date="2020-04-15",
+#         end_date="2020-05-16",
+#         dataset="COPERNICUS/S1_GRD",
+#         bands=["VV", "VH"],
+#         n_images=10,
+#         autostart=False,
+#     )
+#     assert len(limited_tasks2) == 10, "Should be limited to 10 images in time period"
 
-    sentinel2_tasks = earth_engine_to_google_drive(
-        point=point,
-        bounding_box=bbox,
-        start_date="2020-04-15",
-        end_date="2020-05-25",
-        dataset="COPERNICUS/S2_SR",
-        bands=["B8", "B4", "B3"],
-        n_images=10,
-        drive_folder="Vegetation",
-        task_name="b8-b4-b3",
-        autostart=False,
-    )
-    assert len(sentinel2_tasks) == 10, "Should also be limited to 10 images"
+#     sentinel2_tasks = earth_engine_to_google_drive(
+#         point=point,
+#         bounding_box=bbox,
+#         start_date="2020-04-15",
+#         end_date="2020-05-25",
+#         dataset="COPERNICUS/S2_SR",
+#         bands=["B8", "B4", "B3"],
+#         n_images=10,
+#         drive_folder="Vegetation",
+#         task_name="b8-b4-b3",
+#         autostart=False,
+#     )
+#     assert len(sentinel2_tasks) == 10, "Should also be limited to 10 images"
